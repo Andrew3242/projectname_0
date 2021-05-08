0b714a6b955b76be6c029797b4079327a15f1dd3
Andrew3242 <ahlestin.and@yandex.ru>
2021-05-08 10:17:56 +0300

    Third commit

---
+++ untitled/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println("HelloWorld!");
+        System.out.println("SecondCommit!");
+        System.out.println("ThirdCommit!");
+    }
+}
